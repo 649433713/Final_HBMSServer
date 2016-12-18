aee2232cf61787675eca094bca204c84db74783a
649433713 <649433713@qq.com>
2016-12-18 19:35:08 +0800

    数据库2.9

--- HBMSServer/src/test/java/testDataHelper/AddUser.java
+++ HBMSServer/src/test/java/testDataHelper/AddUser.java
@@ -14,7 +14,7 @@ public class AddUser {
 		UserDao userDao = new UserDaoImpl();
 		try {
 
-			userDao.addUser(new UserPO(0, UserType.WebManager, "小俊2", "66666666", "也是小俊", "110",null, 0,MemberType.Tourist, null,0, null,120));
+			userDao.addUser(new UserPO(0, UserType.WebMarketer, "小俊3", "66666666", "也是小俊", "110",null, 0,MemberType.Tourist, null,0, null,120));
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
