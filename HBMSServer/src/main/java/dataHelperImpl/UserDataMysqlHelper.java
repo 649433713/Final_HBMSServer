fc411e6b8907d71002f5f1570a168d04e98fa9c0
649433713 <649433713@qq.com>
2017-01-02 01:38:29 +0800

    --

--- HBMSServer/src/main/java/dataHelperImpl/UserDataMysqlHelper.java
+++ HBMSServer/src/main/java/dataHelperImpl/UserDataMysqlHelper.java
@@ -302,8 +302,8 @@ public class UserDataMysqlHelper implements UserDataHelper {
                 preparedStatement.setString(4,userPO.getPassword());
                 preparedStatement.setString(5,userPO.getName());
                 preparedStatement.setString(6,userPO.getContact());
-                String portraitPath=imageHelper.makeUserDir(userID)+"/portrait"+userID+".jpg";
-                preparedStatement.setString(7,portraitPath);
+                String portraitPath=imageHelper.getUserDir(userID).getPath().replace('\\','/')+"/portrait"+userID+".jpg";
+                 preparedStatement.setString(7,portraitPath);
                 preparedStatement.setLong(8,userPO.getCreditValue());
                 preparedStatement.setInt(9,userPO.getMemberType().ordinal());
                 preparedStatement.setString(10,userPO.getMemberInfo());
