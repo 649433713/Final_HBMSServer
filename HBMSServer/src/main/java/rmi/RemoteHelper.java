18bd1a4e905607782262588aae2b608c17627cb0
alex2097 <yym19960930@163.com>
2017-01-02 16:27:57 +0800

    commit ver 0.5.2.4 temp commit

--- HBMSServer/src/main/java/rmi/RemoteHelper.java
+++ HBMSServer/src/main/java/rmi/RemoteHelper.java
@@ -19,9 +19,9 @@ public class RemoteHelper {
 		try {
 			dataRemoteObject = new DataRemoteObject();
 			LocateRegistry.createRegistry(8888);
-			Naming.rebind("rmi://192.168.43.168:8888/DataRemoteObject",dataRemoteObject);
+			//Naming.rebind("rmi://192.168.43.168:8888/DataRemoteObject",dataRemoteObject);
 			
-			//Naming.rebind("rmi://127.0.0.1:8888/DataRemoteObject",dataRemoteObject);
+			Naming.rebind("rmi://127.0.0.1:8888/DataRemoteObject",dataRemoteObject);
 		} catch (RemoteException e) {
 			e.printStackTrace();
 		} catch (MalformedURLException e) {
